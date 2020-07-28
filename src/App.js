06debae128daf872d3455385241ac70517d3a395
nevty <nevtygen@gmail.com>
2020-07-28 18:12:59 +0700

    Added Layout from ant-design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, {useState} from 'react';
+import {Route} from "react-router-dom";
 import './App.css';
 import MainBoard from "./Component/MainBoard/MainBoard";
-import {Route} from "react-router-dom";
 import DeskBoard from "./Component/DeskBoard/DeskBoard";
+import {Layout, Menu} from "antd";
+import {UserOutlined} from "@ant-design/icons"
+const {Sider,Content,Header,Footer} = Layout;
 
 function App({ history }) {
     const [collapse,toggleCollapse] = useState(false);
     const handleCollapse = collapsed=>toggleCollapse(collapsed);
     return (
-        <div className="Layout">
-            <div className="Layout">
-                <div className="Sider">
-                    <div className="Menu">
-
-                    </div>
-                </div>
-                <div className="Content">
+        <Layout className="App">
+            <Layout>
+                <Content className="Content">
                     <Route path="/" exact component={MainBoard}/>
                     <Route path="/desk/:id" component={DeskBoard}/>
-                </div>
-            </div>
-        </div>
+                </Content>
+            </Layout>
+            <Sider collapsible collapsed={collapse} onCollapse={handleCollapse} className="Sider">
+                <Menu theme="dark" className="Menu" mode="inline">
+                    <Menu.Item icon={<UserOutlined/>}>
+                        Profile
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+        </Layout>
     );
 }
 
