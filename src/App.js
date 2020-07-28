1f27e118331f3ac4127f8c788994f9ebd47caf61
nevty <theacutemace@gmail.com>
2020-07-28 08:58:56 +0700

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import MainBoard from "./Component/MainBoard/MainBoard";
+import {Route} from "react-router-dom";
+import DeskBoard from "./Component/DeskBoard/DeskBoard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ history }) {
+    const [collapse,toggleCollapse] = useState(false);
+    const handleCollapse = collapsed=>toggleCollapse(collapsed);
+    return (
+        <div className="Layout">
+            <div className="Layout">
+                <div className="Sider">
+                    <div className="Menu">
+
+                    </div>
+                </div>
+                <div className="Content">
+                    <Route path="/" exact component={MainBoard}/>
+                    <Route path="/desk/:id" component={DeskBoard}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
