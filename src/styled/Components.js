4e7de9589e80ba148d464b3ebf2cc7b4b0ee3776
nevty <theacutemace@gmail.com>
2020-07-28 09:10:10 +0700

    Style components

---
+++ src/styled/Components.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    max-width: 230px;
+    min-height: 80px;
+    border-radius: 5px;
+    border: 1px solid black
+`
+
+export const Button = styled.button`
+    display: inline-block;
+    color: black;
+    border: 2px solid black;
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border-radius: 5px;
+`
