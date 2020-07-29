d81dd0dd6ed154991a1e94bc12f4e593ff99b300
nevty <nevtygen@gmail.com>
2020-07-29 18:18:53 +0700

    Adapted Sider Menu for mobile && added Login Page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React, {useState} from 'react';
-import {Route} from "react-router-dom";
-import './App.css';
+import {Link, Route} from "react-router-dom";
 import MainBoard from "./Component/MainBoard/MainBoard";
 import DeskBoard from "./Component/DeskBoard/DeskBoard";
-import {Layout, Menu} from "antd";
-import {UserOutlined} from "@ant-design/icons"
-const {Sider,Content,Header,Footer} = Layout;
+import {Grid, Layout, Menu} from "antd";
+import {LayoutOutlined, UserOutlined} from "@ant-design/icons"
+import LoginPage from "./pages/LoginPage";
+import './App.css';
+
+const {Sider,Content} = Layout;
+const {useBreakpoint} = Grid;
 
 function App({ history }) {
-    const [collapse,toggleCollapse] = useState(false);
+    const screens = useBreakpoint();
+    const [collapse,toggleCollapse] = useState(true);
     const handleCollapse = collapsed=>toggleCollapse(collapsed);
     return (
         <Layout className="App">
             <Layout>
-                <Content className="Content">
+                <Content style={screens.sm ? null : {minWidth: "320px"}} className="Content">
                     <Route path="/" exact component={MainBoard}/>
                     <Route path="/desk/:id" component={DeskBoard}/>
+                    <Route path="/login" exact component={LoginPage}/>
                 </Content>
             </Layout>
-            <Sider collapsible collapsed={collapse} onCollapse={handleCollapse} className="Sider">
+            <Sider collapsedWidth={screens.sm ? "80" : "0"} reverseArrow collapsible collapsed={collapse} onCollapse={handleCollapse} className="Sider" >
                 <Menu theme="dark" className="Menu" mode="inline">
                     <Menu.Item icon={<UserOutlined/>}>
-                        Profile
+                        <Link to="/login">Profile</Link>
+                    </Menu.Item>
+                    <Menu.Item icon={<LayoutOutlined/>}>
+                        <Link to="/">Boards</Link>
                     </Menu.Item>
                 </Menu>
             </Sider>
