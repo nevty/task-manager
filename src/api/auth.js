00b137e41ddbcb5388c9ac5ce4bfe6354a14632b
nevty <nevtygen@gmail.com>
2020-08-07 18:04:01 +0700

    Added auth via Google

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,31 +1,52 @@
 import firebase from "firebase/app";
+import {provider} from "../firebaseConfig";
+
 const authAPI = {
-    async login(email,password) {
-        try{
-          await firebase.auth().signInWithEmailAndPassword(email,password)
+    async login(email, password) {
+        try {
+            await firebase.auth().signInWithEmailAndPassword(email, password)
         } catch (e) {
             throw e
         }
     },
-    logout(){
-      try{
-          firebase.auth().signOut()
-      } catch (e) {
-        throw e
-      }
+    logout() {
+        try {
+            firebase.auth().signOut()
+        } catch (e) {
+            throw e
+        }
     },
-    async register(name,email,password) {
-        try{
-            await firebase.auth().createUserWithEmailAndPassword(email,password)
+    async register(name, email, password) {
+        try {
+            await firebase.auth().createUserWithEmailAndPassword(email, password)
             const uid = this.getUid();
             await firebase.database().ref(`/users/${uid}/info`).set({
-                name
+                name,
+                picture: ''
             })
         } catch (e) {
             throw e
         }
     },
-    getUid(){
+    async loginWithPopUp(method) {
+        switch (method) {
+            case "google": {
+                await firebase.auth().signInWithPopup(provider)
+                    .then(({additionalUserInfo}) => {
+                        if (additionalUserInfo && additionalUserInfo.isNewUser) {
+                            const uid = this.getUid();
+                            firebase.database().ref(`/users/${uid}/info`).set({
+                                name: additionalUserInfo.profile.given_name,
+                                picture: additionalUserInfo.profile.picture
+                            })
+                        }
+                    })
+                    .catch(e=>console.log(e))
+            }
+        }
+
+    },
+    getUid() {
         const user = firebase.auth().currentUser;
         return user ? user.uid : null
     },
